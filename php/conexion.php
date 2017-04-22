31abb85f4cc8fe40bc60f5f36fdf1cad67aac0cd
felipechaux <felipechauxx@misena.edu.co>
2017-04-22 17:27:29 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -3,7 +3,7 @@
 class conexion{
   
 	  function dbconexion(){
-          ////////////////////////////conexino --> host , user , password , y nombre de la base de datos .
+          ////////////////////////////conexion --> host , user , password , y nombre de la base de datos .
 	  $conexion = mysqli_connect("sql309.eshost.com.ar", "eshos_19709769", "pipecha", "eshos_19709769_bussinesvote");
       $conexion->set_charset("utf8");
 	  if (mysqli_connect_errno($conexion)) {
